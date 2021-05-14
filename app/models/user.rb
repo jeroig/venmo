767ef0f2a55af0aea3fa4e0e5987f7ea6be0edda
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-14 16:22:55 -0300

    Add schema information on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  nick       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :nick, presence: true
