90958b21eb57a562fc0a5030b8310a806906fe0f
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-14 18:48:48 -0300

    (WIP) Define routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+      post 'user/:id/payment', to: 'users#payment', as: 'payment'
+      get  'user/:id/feed', to: 'users#feed', as: 'feed'
+      get  'user/:id/balance', to: 'users#balance', as: 'balance'
+    end
+  end
 end
