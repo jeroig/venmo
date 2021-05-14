47b4b298c1ac47fe15cbc910381383a916753e76
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-14 16:02:54 -0300

    Create User validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    subject { create(:user) }
+  subject { create(:user) }
 
+  describe 'validations' do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_presence_of(:nick) }
   end
 
-  # describe 'email' do
-  #   it { is_expected_to_not allow_value('blah').for(:email) }
-  #   it { is_expected_to allow_value('a@b.com').for(:email) }
-  # end
+  describe 'email' do
+    it 'has a invalid email' do
+      subject.email = 'invalid_email'
+      subject.validate
+      expect(subject.errors[:email]).to include('is invalid')
+    end
+  end
 end
