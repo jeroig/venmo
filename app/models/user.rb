ce11eea924424ba9b1b07b3048d9bd9b38a24814
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-14 16:12:40 -0300

    Test pass on Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :nick, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_presence_of(:nick) }
   end
 
