8e4736148cda00b7f774339067125f2c484f31c1
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-14 17:18:34 -0300

    Use should instead of expected_to

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:amount) }
-    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0).is_less_than(1_000) }
+    it { should validate_presence_of(:amount) }
+    it { should validate_numericality_of(:amount).is_greater_than(0).is_less_than(1_000) }
   end
 
   describe 'associations' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe User, type: :model do
   subject { create(:user) }
 
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-    it { is_expected.to validate_presence_of(:nick) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:nick) }
   end
 
   describe 'email' do
