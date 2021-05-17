232dc3c2e549e4761b30c2c53d3cccf92925bea5
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-17 12:26:45 -0300

    BugFix on routes names

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   module V1
-    class UsersController < ApplicationController
+    class UserController < ApplicationController
       include Pagy::Backend
       before_action :set_user
 
