6988712d05452531d9f3fc8a741e1f7f329fbd72
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-17 10:59:20 -0300

    Balance check endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,17 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :set_user
+
+      def balance
+        render json: @user.to_json(only: [:balance]), status: :ok
+      end
+
+      private
+
+      def set_user
+        @user = User.find(params[:id])
+      end
     end
   end
 end
