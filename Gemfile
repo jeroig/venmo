e6c41990e0b430373d2a7d9802f72d3fdf12b20b
Juan Eduardo Roig <jeroig@gmail.com>
2021-05-17 11:39:27 -0300

    (WIP) Feed endpoint

--- Gemfile
+++ Gemfile
@@ -49,4 +49,5 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'pagy', '~> 3.13'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,16 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      include Pagy::Backend
+
+      def feed
+        # (WIP) Just for activate the endpoint, list all Payments without
+        # any condition in order to setup the pagination
+        pagy, payments = pagy(Payment.includes(:sender, :receiver))
+        response = { meta: pagy_metadata(pagy), data: [] }
+        response[:data] = payments.map { |payment| { title: payment.title } }
+        render json: response.to_json
+      end
     end
   end
 end
